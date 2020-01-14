3d7dd26e3d6b8f295a6cea4e38813934e874ed8c
jorgedure <53130457+jorgedure@users.noreply.github.com>
2020-01-14 17:27:29 -0300

    module 8.74

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -9,19 +9,28 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from '_services/error.interceptor';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';   
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
 
 @NgModule({
    declarations: [
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      ListsComponent,
+      MemberListComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
--- DatingApp.API/Models/User.cs
+++ DatingApp.API/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace DatingApp.API.Models
 {
     public class User
@@ -6,5 +9,16 @@ namespace DatingApp.API.Models
         public string UserName {get;set;}
         public byte[] HashPassword {get; set;} 
         public byte[] SaltPassword {get; set;}
+        public string Gender { get; set; }
+        public DateTime DateOfBirdh { get; set; }
+        public string KnowsnAs { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookinFor { get; set; }
+        public string Intereses { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; }
     }
 }
